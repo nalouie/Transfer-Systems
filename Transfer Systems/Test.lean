846effed83a177eb042a82a8c9c226d3734a78d3
Nathan Louie <nalouie@uw.edu>
2024-02-06 19:04:26 -0800

    defined transfer systems

--- Transfer Systems/Test.lean
+++ Transfer Systems/Test.lean
@@ -1,3 +1,10 @@
-import Mathlib.Topology.Basic
+import Mathlib.Order.Lattice
+import Mathlib.Order.Sublattice
 
-#check TopologicalSpace
+universe u v
+
+structure TransferSystem (P : Type u) [base_struct: Lattice P] where
+  carrier : Set P
+  poset_struct : PartialOrder carrier
+  refine: ∀ a b : carrier, poset_struct.le a b → base_struct.le a b
+  restrict: ∀ a b c: carrier, poset_struct.le a b ∧ base_struct.le c b → base_struct.le (base_struct.inf a c) c
