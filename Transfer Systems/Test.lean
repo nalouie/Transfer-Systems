cf900b04f99144ee39a74f9ed4ea1adcd47c40c0
Nathan Louie <nalouie@uw.edu>
2024-02-06 19:11:44 -0800

    added documentation for T.S.

--- Transfer Systems/Test.lean
+++ Transfer Systems/Test.lean
@@ -3,6 +3,10 @@ import Mathlib.Order.Sublattice
 
 universe u v
 
+-- Given a lattice (P, ≤),
+-- a transfer system on (P,≤) is a partial order (P,◁) that satisfies
+-- 1) refinement: ∀ a, b ∈ P, if a ◁ b, then a ≤ b
+-- 2) restrict: ∀ a, b, c ∈ P, if a ◁ b and c ≤ b, then a ∧ c → c
 structure TransferSystem (P : Type u) [base_struct: Lattice P] where
   carrier : Set P
   poset_struct : PartialOrder carrier
